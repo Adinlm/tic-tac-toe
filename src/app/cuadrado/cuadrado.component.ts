d2776e26f263a316929b908955f51ba81782ee72
AdinLM <adin.lopez.mun@gmail.com>
2023-01-09 14:36:24 -0300

    logica componente cuadrado

--- src/app/cuadrado/cuadrado.component.ts
+++ src/app/cuadrado/cuadrado.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { JuegoService } from '../juego.service';
 
 @Component({
   selector: 'app-cuadrado',
   templateUrl: './cuadrado.component.html',
   styleUrls: ['./cuadrado.component.css']
 })
-export class CuadradoComponent {
+export class CuadradoComponent implements OnInit {
+
+  constructor(public juegoService: JuegoService) { }
+
+  @Input() cuadrado: any;
+
+  ngOnInit(): void {
+
+  }
+
+  cambiarJugador() {
+    this.juegoService.elJuegoActivo = true;
+
+    if (this.juegoService.elJuegoActivo && this.cuadrado.state === null) {
+      this.cuadrado.state = this.juegoService.jugadorActivo;
+      this.juegoService.cambiarTurnoJugador(this.cuadrado);
+    }
+
+  }
 
 }
