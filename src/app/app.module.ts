bf7c6df641089db5a19b1baf67f94db717514359
AdinLM <adin.lopez.mun@gmail.com>
2023-01-09 14:23:04 -0300

    componentes cuadrado y tablero creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CuadradoComponent } from './cuadrado/cuadrado.component';
+import { TableroComponent } from './tablero/tablero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuadradoComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule,
--- src/app/juego.service.ts
+++ src/app/juego.service.ts
@@ -13,11 +13,11 @@ export class JuegoService {
   elJuegoTerminado: boolean = false;
   ganador: boolean = false;
 
-  constructor() { 
+  constructor() {
     this.juegoNuevo();
   }
 
-  juegoNuevo(){
+  juegoNuevo() {
     this.jugadorActivo = "X";
     this.contadorTurnos = 0;
     this.elJuegoActivo = false;
@@ -26,63 +26,82 @@ export class JuegoService {
     this.tablero = this.crearTablero();
   }
 
-  crearTablero(){
+  crearTablero() {
     let tablero = [];
-    for (let i = 0; i<9; i++){
-      tablero.push({id:1, state:null});
+    for (let i = 0; i < 9; i++) {
+      tablero.push({ id: 1, state: null });
     };
     return tablero;
   }
 
-  get getTablero(){
+  get getTablero() {
     return this.tablero;
   }
 
-  set setTablero(tablero:any){
+  set setTablero(tablero: any) {
     this.tablero = [...tablero];
   }
 
-  cambiarTurnoJugador(clickCuadrado: any){
+  cambiarTurnoJugador(clickCuadrado: any) {
     this.actualizaTablero(clickCuadrado);
-    if(!this.elJuegoTerminado) this.jugadorActivo = this.jugadorActivo === "X" ? "O" : "X"
+    if (!this.elJuegoTerminado) this.jugadorActivo = this.jugadorActivo === "X" ? "O" : "X"
     this.contadorTurnos++;
     this.elJuegoTerminado = this.elJuegoTerminado ? true : false;
   }
 
-  actualizaTablero(clickCuadrado: any){
+  actualizaTablero(clickCuadrado: any) {
     this.tablero[clickCuadrado.id].state = clickCuadrado.state;
-    if(this.ganador) {
+    if (this.ganador) {
       this.ganador = true;
       this.elJuegoTerminado = false;
       this.elJuegoTerminado = true;
     }
   }
 
-  get juegoAcabado():boolean {
+  get juegoAcabado(): boolean {
     return this.contadorTurnos > 8 || this.ganador ? true : false;
   }
 
-  get esGanador():boolean {
+  get esGanador(): boolean {
     return this.comprobarDiagonal() || this.comprobarColumnas(this.tablero, "row") || this.comprobarColumnas(this.tablero, "col") ? true : false;
   }
-  
-  comprobarColumnas (tablero: any, mode:any){
-    
+
+  comprobarColumnas(tablero: any, mode: any) {
+
     const ROW = mode === "row" ? true : false;
     const DIST = ROW ? 1 : 3;
     const INC = ROW ? 3 : 1;
     const NUMTIMES = ROW ? 7 : 3;
 
-    for ( let i = 0; i<NUMTIMES; i += INC){
-      let primerCuadrado = tablero[i],state;
+    for (let i = 0; i < NUMTIMES; i += INC) {
+      let primerCuadrado = tablero[i], state;
       let segundoCuadrado = tablero[i + DIST].state;
       let tercerCuadrado = tablero[i + (DIST * 2)].state;
 
-      if(primerCuadrado && segundoCuadrado && tercerCuadrado){
-        if(primerCuadrado === segundoCuadrado)
+      if (primerCuadrado && segundoCuadrado && tercerCuadrado) {
+        if (primerCuadrado === segundoCuadrado && segundoCuadrado == tercerCuadrado) return true;
       }
     }
 
+    return false
+
+  }
+
+  comprobarDiagonal() {
+    const timesRun = 2,
+      cuadradoMedio = this.tablero[4].state;
+
+    for (let i = 0; i <= timesRun; i += 2) {
+
+      let esquinaSuperior = this.tablero[i].state;
+      let esquinaInferior = this.tablero[8 - i].state;
+
+      if (cuadradoMedio && esquinaSuperior && esquinaInferior) {
+        if (cuadradoMedio === esquinaSuperior && esquinaSuperior === esquinaInferior) return true;
+      }
+
+    }
+    return false;
   }
 
 }
