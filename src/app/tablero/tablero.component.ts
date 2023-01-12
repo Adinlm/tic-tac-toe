04514c53432ee0a573566655a91f360979b7b83c
AdinLM <adin.lopez.mun@gmail.com>
2023-01-12 12:33:01 -0300

    logica al tablero

--- src/app/tablero/tablero.component.ts
+++ src/app/tablero/tablero.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JuegoService } from '../juego.service';
 
 @Component({
   selector: 'app-tablero',
   templateUrl: './tablero.component.html',
   styleUrls: ['./tablero.component.css']
 })
-export class TableroComponent {
+export class TableroComponent implements OnInit{
+  constructor(public juegoService: JuegoService){ 
+  }
 
+  ngOnInit(): void{
+
+  }
 }
